fe697cadd6cf27835b064238e797b4c8997c5fa5
lex <lex.quarkie@gmail.com>
2022-07-27 23:14:04 +0300

    Move logic to server

--- server/logic.py
+++ server/logic.py
@@ -1,13 +1,20 @@
 from random import random
 
 
+async def init_tickers():
+    tickers_keys = [f"ticker_{num:02d}" for num in range(100)]
+    tickers_dict = dict.fromkeys(tickers_keys, 0)
+
+    return tickers_dict
+
 async def generate_movement():
     movement = -1 if random() < 0.5 else 1
 
     return movement
 
+async def calculate_step(current):
+    deltas = [await generate_movement() for _ in range(100)]
+    new_values = [x + int(y) for x, y in zip(current.values(), deltas)]
+    new_dict = {k: v for k, v in zip(current.keys(), new_values)}
 
-async def calculate_step():
-    step_values = [await generate_movement() for _ in range(100)]
-
-    return step_values
+    return new_dict
--- server/server.py
+++ server/server.py
@@ -2,24 +2,26 @@ import asyncio
 import json
 
 import websockets
+from websockets.exceptions import ConnectionClosedOK
 
-from settings import WEBSOCKET_HOST, WEBSOCKET_PORT
-from logic import calculate_step
+from logic import init_tickers, calculate_step
+
+WEBSOCKET_HOST = "localhost"
+WEBSOCKET_PORT = 8765
 
 connected = set()
-tickers = [0, ] * 100
 
 async def handler(websocket):
+    current_tickers = await init_tickers()
 
     while True:
         connected.add(websocket)
         try:
-            step_values = await calculate_step()
-            message = json.dumps(step_values)
+            current_tickers = await calculate_step(current_tickers)
+            message = json.dumps(current_tickers)
             await asyncio.sleep(1)
-            print(f"sended {message}. Connected: {len(connected)}")
             await websocket.send(message)
-        except KeyboardInterrupt:
+        except (KeyboardInterrupt, ConnectionClosedOK):
             return
         finally:
             connected.remove(websocket)
