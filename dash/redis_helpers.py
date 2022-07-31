e1ed5c292d616bee2c9f6aa8cf1c0983df6ebcaa
lex <lex.quarkie@gmail.com>
2022-07-31 18:08:45 +0300

    Move redis helpers

---
+++ dash/redis_helpers.py
@@ -0,0 +1,19 @@
+import logging
+import time
+
+import redis
+
+def init_redis_conn():
+    conn = redis.from_url("redis://redis", decode_responses=True)
+    conn.set("current_ticker", "ticker_00")
+
+    return conn
+
+
+def load_values(redis, ticker_name):
+    current_ts = int(time.time()) + 1
+    values = redis.execute_command(
+        "TS.RANGE", ticker_name, current_ts - 10000, current_ts
+    )
+
+    return values
